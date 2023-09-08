ff4e99e5fe4853ab63c81c3f82a7e325cebfc59f
reynierlabaut <labautserranoreynier@gmail.com>
2023-09-08 15:23:30 -0300

    add login screen inputs

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,53 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Formik, useField} from 'formik';
+import {Button, StyleSheet, View} from 'react-native';
+import StyledTextInput from '../components/StylesTextInput';
+import StylesText from '../components/StylesText';
+
+const initialValue = {
+  email: '',
+  password: '',
+};
+
+const FormikInputValue = ({name, ...props}) => {
+  const [field, meta, helpers] = useField(name);
+  return (
+    <>
+      <StyledTextInput
+        value={field.value}
+        onChangeText={value => helpers.setValue(value)}
+        {...props}
+      />
+      {meta.error && <StylesText style={styles.error}></StylesText>}
+    </>
+  );
+};
 
 export default function LogInPage() {
-  return <Text>working on it</Text>;
+  return (
+    <Formik
+      initialValues={initialValue}
+      onSubmit={values => console.log(values)}
+      validate={validate}>
+      {({handleSubmit}) => {
+        return (
+          <View style={styles.form}>
+            <FormikInputValue name="email" placeholder="E-mail" />
+            <FormikInputValue
+              name="password"
+              placeholder="Password"
+              secureTextEntry
+            />
+            <Button onPress={handleSubmit} title="Login" />
+          </View>
+        );
+      }}
+    </Formik>
+  );
 }
+
+const styles = StyleSheet.create({
+  form: {
+    margin: 15,
+  },
+});
